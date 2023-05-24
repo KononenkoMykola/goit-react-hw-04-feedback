687fc6d9f21e541f71f62fe855a89a39993f3c88
KononenkoMykola <114486496+KononenkoMykola@users.noreply.github.com>
2023-05-24 22:57:24 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { Container } from './App.styled';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Section from './Section';
+import Notification from './Notification';
+// import PropTypes from 'prop-types';
+
+const initOptions = { good: 0, neutral: 0, bad: 0 };
+
+export default function App() {
+  const [options, setOptions] = useState(initOptions);
+  const { good, neutral, bad } = options;
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const total = countTotalFeedback();
+
+  const onLeaveFeedback = feedbackType => {
+    setOptions(prev => ({ ...prev, [feedbackType]: prev[feedbackType] + 1 }));
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Container>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
+      </Container>
+      <Container>
+        <Section title="Statistics">
+          {!total ? (
+            <Notification message={'There is no feedback'} />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </Container>
+    </>
   );
-};
+}
